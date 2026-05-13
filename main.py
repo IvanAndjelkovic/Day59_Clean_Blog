b119a2e320bae4d952bb1e413a93b41a11d3944f
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-05-13 06:03:49 +0000

    Holding of the JSON data from teh API

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, url_for
+import requests
+
 
 
 app=Flask(__name__)
 
+blog_url="https://api.npoint.io/bd3389b6ea02b071067c"
+response=requests.get(blog_url)
+blog_post_json=response.json()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    
+    return render_template("index.html", blogs=blog_post_json)
+
+# @app.post("/")
+# def home(blog_number):
+#     blog_number=int(blog_number) - 1
+#     # # if int(blog_number) > len(blog_post_json):
+#     #     pass
+#     # else:
+#     #     return render_template("index.html")
+
 
 
 @app.route("/about")
@@ -16,6 +33,8 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
 
 
 if __name__=="__main__":
